b46801baddae9337a06e2facf59bac3d1aab2457
goodjeon <513ehdwnsl@gmail.com>
2022-09-13 00:12:45 -0700

    start

---
+++ MyFirstiOS/MyFirstiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFirstiOS
+//
+//  Created by dong jun Jeon on 2022/09/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
