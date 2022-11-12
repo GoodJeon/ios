be8919b4b936cfd15749cc501c40d878eca0137a
goodjeon <513ehdwnsl@gmail.com>
2022-11-12 13:14:49 +0900

    Section2

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by dong jun Jeon on 2022/11/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
