63d8c4ab5ca6de912707cfb82f61febe9faeb8ec
goodjeon <513ehdwnsl@gmail.com>
2022-11-13 13:39:09 +0900

    I AM Poor

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by dong jun Jeon on 2022/11/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
